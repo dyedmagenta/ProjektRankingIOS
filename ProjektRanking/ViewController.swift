a6d68b23087c7cc4c9ea4479903d8c21cc910ae2
P1entaszek <piotrjaszczurowski87@gmail.com>
2017-03-29 10:18:15 +0200

    Initial Commit

---
+++ ProjektRanking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjektRanking
+//
+//  Created by MAC-10 on 29.03.2017.
+//  Copyright © 2017 PitPit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
